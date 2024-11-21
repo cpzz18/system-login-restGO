862dfc07966b8ff408416f5a45053bb9132d614a
user18 <robbincfa@gmail.com>
2024-11-21 14:58:46 +0700

    modify route

--- routes/routes.go
+++ routes/routes.go
@@ -12,23 +12,14 @@ import (
 
 func Route() {
 	r := mux.NewRouter()
-	// Route untuk register dan login 
 	r.HandleFunc("/register", controllers.RegisterUser).Methods("POST")
 	r.HandleFunc("/login", controllers.LoginUser).Methods("POST")
-
-	r.HandleFunc("/users", controllers.GetUsers).Subrouter()
-	r.Use(middleware.ValidateToken)
-
-	// Route CRUD untuk users dengan autentikasi
-	r.HandleFunc("/", controllers.GetUser).Methods("GET")
-	r.HandleFunc("/{id}", controllers.GetUser).Methods("GET")
-	r.HandleFunc("/{id}", controllers.UpdateUser).Methods("PUT")
-	r.HandleFunc("/{id}", controllers.DeleteUser).Methods("delete")
+	r.HandleFunc("/users", controllers.GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", controllers.GetUser).Methods("GET")
+	r.HandleFunc("/users/{id}", controllers.UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", controllers.DeleteUser).Methods("delete")
 	r.Use(middleware.LoggingMiddleware)
 
-	// Menjalankan server dan menangani error jika gagal
-	log.Println("server run on port 8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatal("Server Failed to start", err)
-	}
+	log.Println("server run")
+	http.ListenAndServe(":8000", r)
 }
